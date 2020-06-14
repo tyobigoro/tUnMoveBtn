daf3567d9eb4eb8fab8065562f14dffeb573e9fa
tyobigoro_i <tyobigorox@gmail.com>
2020-06-14 22:52:49 +0900

    c

--- tUnMoveBtn/ViewController.swift
+++ tUnMoveBtn/ViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var array = [String](repeating: "A", count: 10)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func addA(_ sender: Any) {
+        array.append("A")
+        tableView.insertRows(at: [IndexPath(row: array.count - 1, section: 0)], with: .right)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = array[indexPath.row]
+        return cell
+    }
 
 }
 
